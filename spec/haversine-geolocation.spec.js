9dbef337c427e299c3757e94701c484b049f1f16
Daniil Sydorenko <daniildeveloper@gmail.com>
2017-07-03 20:37:01 +0200

    Jasmine test fixed

--- spec/haversine-geolocation.spec.js
+++ spec/haversine-geolocation.spec.js
@@ -3,61 +3,46 @@
 const geolocation = require('../index');
 
 describe("Test for Haversine-Geolocation", () => {
-
-    // TODO test: isGeolocationAvilable, getClosestPosition, important: Current position
-
     let distance = null,
         distanceBetween = null,
         closestPosition = null;
 
     beforeEach(() => {
-        const positionsToCompare = [
+        const points = [
             {
-                id: 1,
                 latitude: 61.5322204,
                 longitude: 28.7515963
             },
             {
-                id: 2,
                 latitude: 51.9971208,
                 longitude: 22.1455439
             },
             {
-                id: 3,
                 latitude: 45.3571207,
                 longitude: 30.3435456
             },
             {
-                id: 4,
                 latitude: 51.2971208,
                 longitude: 22.0455439
             },
             {
-                id: 5,
                 latitude: 62.0971208,
                 longitude: 13.0455439
             },
         ];
 
         distance = geolocation.haversine(
-            positionsToCompare[0].latitude,
-            positionsToCompare[0].longitude,
-            positionsToCompare[1].latitude,
-            positionsToCompare[1].longitude
+            points[0].latitude,
+            points[0].longitude,
+            points[1].latitude,
+            points[1].longitude
         );
 
-        distanceBetween = geolocation.getDistanceBetween({
-            latitude: positionsToCompare[0].latitude,
-            longitude: positionsToCompare[0].longitude,
-        },{
-            latitude: positionsToCompare[0].latitude,
-            longitude: positionsToCompare[0].longitude
-        });
-
-        closestPosition = geolocation.getClosestPosition(positionsToCompare[0], positionsToCompare);
+        distanceBetween = geolocation.getDistanceBetween(points[0], points[1], 'mi');
+        closestPosition = geolocation.getClosestPosition(points[0], points);
     });
 
     it("haversine: value type number", () => expect(distance).toEqual(jasmine.any(Number)));
-    it("getDistanceBetween: value type number", () => expect(distanceBetween).toEqual(jasmine.any(Number)));
+    it("getDistanceBetween: greater than zero", () => expect(distanceBetween).toBeGreaterThan(0));
     it("closestPosition: value type object", () => expect(closestPosition).toEqual(jasmine.any(Object)));
 });
